fabe9afe78d75bb6344f11afa9e6ed5542285346
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2020-10-03 15:01:54 +0500

    implement lines detection

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import math
 import sys
 
 import cv2 as cv2
+import numpy as np
 
 from DataLoader import TestImagesLoader
 
 
 def _find_contours_with_kernel(img, kernel):
-    morphed = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=2)
+    morphed = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=1)
 
     cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return cnts[0] if len(cnts) == 2 else cnts[1]
@@ -15,20 +17,48 @@ def _find_contours_with_kernel(img, kernel):
 def find_contours(img):
     # https://stackoverflow.com/a/57531326/9577873
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-    close = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=2)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    close = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=1)
 
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (35, 2))
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (45, 2))
     horizontal = _find_contours_with_kernel(close, horizontal_kernel)
 
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 35))
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 45))
     vertical = _find_contours_with_kernel(close, vertical_kernel)
 
     return horizontal + vertical
 
 
-def filter_contours(contours):
-    return contours
+def get_line_length(line):
+    a, b = line[0], line[1]
+    return math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+
+
+def get_line_midpoint(line):
+    a, b = line[0], line[1]
+    return (int(a[0] + b[0]) // 2), int((a[1] + b[1]) // 2)
+
+
+def get_centerline(box):
+    pts = cv2.boxPoints(box)
+
+    box_lines = [(pts[i], pts[(i + 1) % len(pts)]) for i in range(len(pts))]
+    bottom, right, top, left = box_lines
+
+    if get_line_length(left) < get_line_length(top):
+        lines = (left, right)
+    else:
+        lines = (top, bottom)
+
+    return tuple([get_line_midpoint(x) for x in lines])
+
+
+def get_lines(contours):
+    boxes = [cv2.minAreaRect(contour) for contour in contours]
+
+    centerlines = [get_centerline(box) for box in boxes]
+
+    return centerlines
 
 
 def doStuff(img):
@@ -41,9 +71,19 @@ def doStuff(img):
     edges = cv2.Canny(gray, 170, 255)
 
     cnts = find_contours(edges)
-    cnts = filter_contours(cnts)
+    lines = get_lines(cnts)
+
+    """
     for c in cnts:
-        cv2.drawContours(img, [c], -1, (36, 255, 12), 1)
+        box = cv2.minAreaRect(c)
+        pts = cv2.boxPoints(box)
+        for i in range(len(pts)):
+            b = (i + 1) % len(pts)
+            cv2.line(img, tuple(pts[i]), tuple(pts[b]), (0, 0, 255))
+    """
+
+    for pt1, pt2 in lines:
+        cv2.line(img, pt1, pt2, (255, 0, 255))
 
     cv2.imshow("test", img)
     cv2.imshow("edges", edges)
