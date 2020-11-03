7142a0b506f2b391d39057286e30b364e7bb328a
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2020-11-03 22:36:12 +0500

    separate lines from text

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@ struct Word {
     std::string text;
 };
 
+struct Line {
+    cv::Point a, b;
+};
+
 cv::Mat pix8ToMat(Pix *pix) {
     int width = pixGetWidth(pix);
     int height = pixGetHeight(pix);
@@ -30,9 +34,13 @@ cv::Mat pix8ToMat(Pix *pix) {
     return mat;
 }
 
-void drawWords(cv::Mat  &img, std::vector<Word> const &words) {
+void drawWords(cv::Mat &img, std::vector<Word> const &words, std::vector<Line> const &lines) {
     for (auto const &word : words) {
         cv::rectangle(img, word.rect, {255, 0, 0}, 2);
+    }
+
+    for (auto const &line : lines) {
+        cv::line(img, line.a, line.b, {0, 0, 255}, 2);
     }
 
     cv::rotate(img, img, cv::ROTATE_90_COUNTERCLOCKWISE);
@@ -54,33 +62,59 @@ int main(int argc, char *argv[]) {
     }
 
     std::vector<Word> words;
+    std::vector<Line> lines;
 
     api->SetPageSegMode(tesseract::PSM_AUTO_OSD);
+
+    for (auto const &var : std::vector<std::string>
+            {
+            "textord_tabfind_find_tables",
+            "textord_tablefind_recognize_tables"
+            }) {
+        api->SetVariable(var.c_str(), "true");
+    }
 
     PIX *pix = pixRead("../data/0/01.png");
     api->SetImage(pix);
 
     api->Recognize(nullptr);
 
-    tesseract::ResultIterator* ri = api->GetIterator();
+    tesseract::ResultIterator *ri = api->GetIterator();
     tesseract::PageIteratorLevel level = tesseract::RIL_WORD;
     if (ri != nullptr) {
         do {
-            const char* word = ri->GetUTF8Text(level);
+            const char *text = ri->GetUTF8Text(level);
             float conf = ri->Confidence(level);
             int x1, y1, x2, y2;
             ri->BoundingBox(level, &x1, &y1, &x2, &y2);
 
-            words.emplace_back(Word{cv::Rect(cv::Point(x1, y1), cv::Point(x2, y2)), word});
+            cv::Rect rect(cv::Point(x1, y1), cv::Point(x2, y2));
 
-            printf("word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
-                   word, conf, x1, y1, x2, y2);
-            delete[] word;
+            PolyBlockType blockType = ri->BlockType();
+            if (blockType == PolyBlockType::PT_HORZ_LINE) {
+                cv::Point a(rect.x + rect.width / 2, rect.y);
+                cv::Point b(rect.x + rect.width / 2, rect.y + rect.height);
+                lines.emplace_back(Line{a, b});
+            } else if (blockType == PolyBlockType::PT_VERT_LINE) {
+                cv::Point a(rect.x, rect.y + rect.height / 2);
+                cv::Point b(rect.x + rect.width, rect.y + rect.height / 2);
+                lines.emplace_back(Line{a, b});
+            } else if (/*blockType == PolyBlockType::PT_FLOWING_TEXT
+                        || blockType == PolyBlockType::PT_HEADING_TEXT
+                        || blockType == PolyBlockType::PT_PULLOUT_TEXT
+                        || */blockType == PolyBlockType::PT_TABLE) {
+                words.emplace_back(Word{rect, text});
+            }
+
+            printf("text: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
+                   text, conf, x1, y1, x2, y2);
+
+            delete[] text;
         } while (ri->Next(level));
     }
 
     cv::Mat img = pix8ToMat(pix);
-    drawWords(img, words);
+    drawWords(img, words, lines);
     cv::imshow("img", img);
     while (cv::waitKey(0) != 27) {}
 
