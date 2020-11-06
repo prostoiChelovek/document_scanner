e96aa53fc672972ccd34e79b423eea60d7bffd7e
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2020-11-06 15:28:54 +0500

    add a basic image preprocessing

--- main.cpp
+++ main.cpp
@@ -15,25 +15,6 @@ struct Line {
     cv::Point a, b;
 };
 
-cv::Mat pix8ToMat(Pix *pix) {
-    int width = pixGetWidth(pix);
-    int height = pixGetHeight(pix);
-
-    cv::Mat mat(cv::Size(width, height), CV_8UC3);
-
-    for (uint32_t y = 0; y < height; ++y) {
-        for (uint32_t x = 0; x < width; ++x) {
-            l_int32 r, g, b;
-            pixGetRGBPixel(pix, x, y, &r, &g, &b);
-
-            cv::Vec3b color(b, g, r);
-            mat.at<cv::Vec3b>(cv::Point(x, y)) = color;
-        }
-    }
-
-    return mat;
-}
-
 void drawWords(cv::Mat &img, std::vector<Word> const &words, std::vector<Line> const &lines) {
     for (auto const &word : words) {
         cv::rectangle(img, word.rect, {255, 0, 0}, 2);
@@ -47,14 +28,22 @@ void drawWords(cv::Mat &img, std::vector<Word> const &words, std::vector<Line> c
 
     for (auto const &word : words) {
         cv::Point textOrig = {word.rect.y, img.size().height - word.rect.x - word.rect.width / 2 + 5};
-        cv::putText(img, word.text, textOrig, cv::FONT_HERSHEY_COMPLEX, 0.7,
-                    {0, 255, 0}, 1);
+        cv::putText(img, word.text, textOrig, cv::FONT_HERSHEY_COMPLEX, 1.4,
+                    {0, 255, 0}, 2);
     }
+}
+
+void preprocess(cv::Mat &img) {
+    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+
+    cv::fastNlMeansDenoising(img, img, 10, 7, 21);
+
+    cv::threshold(img, img, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
 }
 
 int main(int argc, char *argv[]) {
     auto *api = new tesseract::TessBaseAPI();
-    if (api->Init("/usr/share/tesseract-ocr/5/tessdata", "rus+eng")) {
+    if (api->Init("/usr/share/tesseract-ocr/4.00/tessdata/", "rus+eng")) {
         api->End();
 
         std::cerr << "Cannot initialize tesseract" << std::endl;
@@ -74,8 +63,15 @@ int main(int argc, char *argv[]) {
         api->SetVariable(var.c_str(), "true");
     }
 
-    PIX *pix = pixRead("../data/0/01.png");
-    api->SetImage(pix);
+    cv::Mat img = cv::imread("../data/0/01.png");
+    cv::resize(img, img, img.size() * 2);
+
+    cv::Mat imgDraw = img.clone();
+
+    preprocess(img);
+
+    api->SetImage((uchar*)img.data, img.size().width, img.size().height, img.channels(), img.step1());
+    api->SetSourceResolution(300);
 
     api->Recognize(nullptr);
 
@@ -99,23 +95,25 @@ int main(int argc, char *argv[]) {
                 cv::Point a(rect.x, rect.y + rect.height / 2);
                 cv::Point b(rect.x + rect.width, rect.y + rect.height / 2);
                 lines.emplace_back(Line{a, b});
-            } else if (/*blockType == PolyBlockType::PT_FLOWING_TEXT
+            } else if (blockType == PolyBlockType::PT_FLOWING_TEXT
                         || blockType == PolyBlockType::PT_HEADING_TEXT
                         || blockType == PolyBlockType::PT_PULLOUT_TEXT
-                        || */blockType == PolyBlockType::PT_TABLE) {
+                        || blockType == PolyBlockType::PT_TABLE) {
                 words.emplace_back(Word{rect, text});
-            }
 
-            printf("text: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
-                   text, conf, x1, y1, x2, y2);
+                printf("text: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
+                       text, conf, x1, y1, x2, y2);
+            }
 
             delete[] text;
         } while (ri->Next(level));
     }
 
-    cv::Mat img = pix8ToMat(pix);
-    drawWords(img, words, lines);
-    cv::imshow("img", img);
+    drawWords(imgDraw, words, lines);
+    cv::rotate(img, img, cv::ROTATE_90_COUNTERCLOCKWISE);
+
+    cv::imshow("result", imgDraw);
+    cv::imshow("preprocessed", img);
     while (cv::waitKey(0) != 27) {}
 
     api->End();
